a4fdfe4db92d707e4755577e168ab2e9bec5cf0b
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-02-19 13:47:28 -0300

    criado método create_app, para que quando chamado crie o app, neste caso chame as configurações do arquivo setting, para poder inicializar o app e apoós fazer a migração através da variável migrate que contém o flask Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+from database import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('settings')
+    db.init_app(app)
+    migrate.init_app(app, db)
